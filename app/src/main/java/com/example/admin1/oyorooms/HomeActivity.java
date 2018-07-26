4e013f0d6984dd2b36088cf6379fa841ae15708e
Madhu Gogineni <mchaitanya92@gmail.com>
2018-07-26 20:20:00 +0530

    Added recycler view without alignment

--- app/src/main/java/com/example/admin1/oyorooms/HomeActivity.java
+++ app/src/main/java/com/example/admin1/oyorooms/HomeActivity.java
@@ -1,13 +1,28 @@
 package com.example.admin1.oyorooms;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
+    RecyclerView myRecyclerView;
+    RecyclerView.LayoutManager myRecyclerViewLayoutManager;
+    RecyclerView.Adapter myRecyclerViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +33,15 @@ public class HomeActivity extends AppCompatActivity {
         }
         Window window = HomeActivity.this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        myRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+        myRecyclerViewLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        myRecyclerView.setLayoutManager(myRecyclerViewLayoutManager);
+        List<String> input = new ArrayList<String>();
+        String[] cities = {"Banglore","Coorg","Mysore","Chennai","Delhi","Gurgoan","Hyderabad","Kolkata","Mumbai","Noida","Pune","All Cities"};
+        for(int i=0;i<cities.length;i++) {
+            input.add(cities[i]);
+        }
+        myRecyclerViewAdapter = new MyAdapter(input);
+        myRecyclerView.setAdapter(myRecyclerViewAdapter);
     }
 }
