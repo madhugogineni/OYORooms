980a286a64ee187c0f9cd5083fff863e79f73b74
Madhu Gogineni <mchaitanya92@gmail.com>
2018-07-26 09:59:00 +0530

    search bar edit1

--- app/src/main/java/com/example/admin1/oyorooms/MainActivity.java
+++ app/src/main/java/com/example/admin1/oyorooms/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         }
         Window window = MainActivity.this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
 
         final TextView oyoTextView = (TextView)findViewById(R.id.oyo_text_view);
         final TextView wifiTextView = (TextView)findViewById(R.id.wifi_text_view);
